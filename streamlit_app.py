cc424252cf31d15cff786ba1af6ee79ea27548ef
Jovi ibrahim <124225913+frendly-person@users.noreply.github.com>
2025-01-30 00:40:11 +0000

    Run1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def hitung_luas(Panjang, lebar):
+    return panjang * lebar
+
+def hitung_keliling(panjang, lebar) :
+    return 2 * panjang + lebar
+
+st.title("Menghitung Luas dan Keliling persegi panjang")
+
+panjang = st.number_input("masukan Panjang", min_value=0.0)
+lebar = st.number_input("masukan Lebar", min_value=0.0)
+
+if st.button("Hitung"):
+    Luas = hitung_luas(panjang, lebar)
+    Keliling = hitung_keliling(panjang, lebar)
+    st.write(f"Luas: {Luas}")
+    st.write(f"Keiling: {Keliling}")
